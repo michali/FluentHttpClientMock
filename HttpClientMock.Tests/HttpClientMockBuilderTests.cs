04aacac60953bb3e8d1604e34e1a8280c193acb4
Michali K <michali@michalikons.com>
2020-01-11 02:00:30 +1100

    Renamed a test class

--- HttpClientMock.Tests/HttpClientMockBuilderTests.cs
+++ HttpClientMock.Tests/HttpClientMockBuilderTests.cs
@@ -5,7 +5,7 @@ using System.Net;
 
 namespace HttpClientMock.Tests
 {
-    public class HttpClientWithExpectationsBuilderTests
+    public class HttpClientMockBuilderTests
     {
         [Fact]
         public async Task WhenUriIsSet_ExpectUri()
